754cc74ea49387670c5bf4588bba7c39807f01c7
Natesh Kumar <58300718+Natdev15@users.noreply.github.com>
2022-03-24 23:10:25 +0500

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+    <?php require '_nav.php' ?>
+    <div class="container text-center">
+        <h1 class="text-center" >Logout - Successfully..!</h1>
+
+        <p><a href="./index.php">Login</a></p>
+        <p><a href="./signup.php">Sign-up</a></p>
+    </div>
+</body>
+</html>
